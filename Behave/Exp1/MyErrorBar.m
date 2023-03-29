4df949bdb585ed361b6084fc019fd605e3f7743b
JimmyEsmaily <68420963+JimmyEsmaily@users.noreply.github.com>
2023-03-29 15:10:07 +0200

    Delete Behave directory

--- Behave/Exp1/MyErrorBar.m
+++
@@ -1,9 +0,0 @@
-function MyErrorBar(X,Y,Error,ColorCode,Tikness)
-if nargin<5
-    Tikness=1;
-end
-for i=1:length(X)
-    plot([X(i),X(i)],[(Y(i)-Error(i)),(Y(i)+Error(i))],...
-        'Color',ColorCode,'LineWidth',Tikness)
-end
-end
