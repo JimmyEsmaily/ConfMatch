5b35cbde031b0bf4ca33a254c0a9cc3671f20dcc
JimmyEsmaily <68420963+JimmyEsmaily@users.noreply.github.com>
2023-03-29 15:10:38 +0200

    Delete EEG directory

--- EEG/ERP/Exp2/MyErrorBar_CI.m
+++
@@ -1,9 +0,0 @@
-function MyErrorBar_CI(X,Y,Error1, Error2,ColorCode,Tikness)
-if nargin<5
-    Tikness=1;
-end
-for i=1:length(X)
-    plot([X(i),X(i)],[(Error1(i)),(Error2(i))],...
-        ColorCode,'LineWidth',Tikness)
-end
-end
